ed3f7619a8f45eda450926f968293a035ed07711
zhouyuanmin <1837722596@qq.com>
2023-05-21 22:16:57 +0800

    产品模型

---
+++ goods/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.utils import timezone
+
+
+class BaseModel(models.Model):
+    """自定义Model基类:补充基础字段"""
+
+    create_at = models.DateTimeField(default=timezone.now, verbose_name="创建时间")
+    update_at = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+    delete_at = models.DateTimeField(null=True, default=None, verbose_name="删除时间")
+    note = models.CharField(max_length=255, blank=True, default="", verbose_name="备注")
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return str(self.pk)
+
+    def set_delete(self):
+        """软删除"""
+        self.delete_at = timezone.now()
+        self.save()
+
+
+class Good(BaseModel):
+    part = models.CharField(max_length=255, blank=True, default="", verbose_name="零件号")
+    manufacturer = models.CharField(
+        max_length=255, blank=True, default="", verbose_name="制造商"
+    )  # excel的
+    manufacturer_name = models.CharField(
+        max_length=255, blank=True, default="", verbose_name="制造商名称"
+    )  # 网页上的
+    mfr_part_no = models.CharField(
+        max_length=255, blank=True, default="", verbose_name="制造商零件号"
+    )
+    vendor_part_no = models.CharField(
+        max_length=255, blank=True, default="", verbose_name="供应商零件号"
+    )
+    product_name = models.CharField(
+        max_length=255, blank=True, default="", verbose_name="产品名称"
+    )
+    product_description = models.CharField(
+        max_length=255, blank=True, default="", verbose_name="产品描述"
+    )
+    uoi = models.CharField(
+        max_length=255, blank=True, default="EA", verbose_name="发货单位"
+    )
+    commercial_price_list = models.CharField(
+        max_length=255, blank=True, default="", verbose_name="商业价格表"
+    )
+    msrp = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="制造商建议零售价"
+    )
+    federal_govt_spa = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="联邦政府价格"
+    )
+    ingram_micro_price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="英迈国际价格"
+    )
+    gsa_advantage_price_1 = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="GSA优势价格1"
+    )
+    gsa_advantage_price_2 = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="GSA优势价格2"
+    )
+    gsa_advantage_price_3 = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="GSA优势价格3"
+    )
