269a0df8b4698a545f191cd6d6b2a56570a5e108
zhouyuanmin <1837722596@qq.com>
2023-05-21 20:34:39 +0800

    ec网站账号登陆

--- client/main.py
+++ client/main.py
@@ -30,9 +30,15 @@ base_dir = Path(__file__).resolve().parent
 proxy = "http://127.0.0.1:4780"
 window_width, window_height = (1250, 900)  # 需要根据分辨率来确定窗口大小
 cookies_path = os.path.join(base_dir, "cookies.txt")
+login_email = "lwang@techfocusUSA.com"
+login_password = "Uz#.ZeF8WYqJ3im"
 
 # 页面节点
-page_elements = {}
+page_elements = {
+    "login_email": '//*[@id="inputEmailAddress"]',
+    "login_password": '//*[@id="inputPassword"]',
+    "login_button": '//*[@id="loginBtn"]',
+}
 
 
 # 业务基础函数
@@ -96,5 +102,50 @@ def create_browser():
     driver = get_driver()
     browser = webdriver.Chrome(driver, options=options)
     x, y = random.randint(10, 600), random.randint(10, 20)
+    x, y = 20, 20
     browser.set_window_rect(x, y, width=window_width, height=window_height)
     return browser
+
+
+def login(check=True):
+    browser = create_browser()
+    try:
+        browser.get("https://ec.synnex.com/ecx/login.html")
+        waiting_to_load(browser)
+    except exceptions.TimeoutException as e:
+        logging.warning("打开页面超时,重试一次")
+        browser.get("https://ec.synnex.com/ecx/login.html")
+        waiting_to_load(browser)
+
+    browser.delete_all_cookies()
+    with open(cookies_path, "r") as f:
+        cookies_str = f.read()
+        cookies = json.loads(cookies_str)
+    for cookie in cookies:
+        browser.add_cookie(cookie)
+    browser.get("https://ec.synnex.com/ecx/landing.html")
+
+    if not waiting_to_load(browser, sleep_time=5):
+        browser.get("https://ec.synnex.com/ecx/landing.html")
+        waiting_to_load(browser, sleep_time=5)
+
+    if not check:  # 不判断登录状态
+        return browser
+
+    # 判断登录状态
+    login_buttons = browser.find_elements_by_xpath(page_elements.get("login_email"))
+    if login_buttons:
+        login_email_textbox = browser.find_element_by_xpath(
+            page_elements.get("login_email")
+        )
+        login_email_textbox.send_keys(login_email)
+        login_password_textbox = browser.find_element_by_xpath(
+            page_elements.get("login_password")
+        )
+        login_password_textbox.send_keys(login_password)
+        login_button = browser.find_element_by_xpath(page_elements.get("login_button"))
+        login_button.click()
+        waiting_to_load(browser)
+        return browser
+    else:
+        return browser
