9401a4912644202a3b0e07070d706ea74739408f
zhouyuanmin <1837722596@qq.com>
2023-05-23 15:36:13 +0800

    -

--- client/main.py
+++ client/main.py
@@ -255,7 +255,7 @@ def get_model_param_by_gsa(browser, part):
         valid_source_urls = []
         first_source_urls = []
         for product_div in product_divs:
-            source_divs = product_div.find_element_by_xpath(
+            source_divs = product_div.find_elements_by_xpath(
                 page_elements.get("sources")
             )
             if not source_divs:  # 有些产品,没有sources
