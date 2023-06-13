c83ef28fc70a110db74404e38944544d6e0fa5f4
zhouyuanmin <1837722596@qq.com>
2023-06-13 22:53:57 +0800

    init

--- goods/models.py
+++ goods/models.py
@@ -22,67 +22,18 @@ class BaseModel(models.Model):
         self.save()
 
 
-class Good(BaseModel):
-    part = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="零件号"
-    )  # excel的
-    manufacturer = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="制造商"
-    )  # excel的
-    manufacturer_name = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="制造商名称"
-    )  # gsa网页上的
-    mfr_part_no = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="制造商零件号"
-    )  # ec网页上的
-    vendor_part_no = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="供应商零件号"
-    )  # ec网页上的
-    product_name = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="产品名称"
-    )  # gsa网页上的
-    product_description = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="产品描述"
-    )  # gsa网页上的
-    uoi = models.CharField(
-        max_length=255, blank=True, default="EA", verbose_name="发货单位"
-    )  # 固定值
-    commercial_price_list = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="商业价格表"
-    )  # 不填
-    msrp = models.DecimalField(
-        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="制造商建议零售价"
-    )  # ec网页上的
-    federal_govt_spa = models.DecimalField(
-        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="联邦政府价格"
-    )  # ec网页上的
-    ingram_micro_price = models.DecimalField(
-        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="英迈国际价格"
-    )  # ingram网页上的
-    gsa_advantage_price_1 = models.DecimalField(
-        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="GSA优势价格1"
-    )  # gsa网页上的
-    gsa_advantage_price_2 = models.DecimalField(
-        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="GSA优势价格2"
-    )  # gsa网页上的
-    gsa_advantage_price_3 = models.DecimalField(
-        max_digits=10, decimal_places=2, blank=True, default=0, verbose_name="GSA优势价格3"
-    )  # gsa网页上的
-    coo = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="原产地"
-    )  # gsa网页上的
-    mfr_part_no_gsa = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="制造商零件号"
-    )  # gsa网页上的
+class Brand(BaseModel):
+    name = models.CharField(max_length=255, verbose_name="品牌名称")  # HP or Dell
+    mini_sources = models.IntegerField(default=10, verbose_name="爬虫sources")
+    mini_count = models.IntegerField(default=10, verbose_name="爬虫产品数量")
+    filter_sources = models.IntegerField(default=10, verbose_name="筛选sources")
+    filter_count = models.IntegerField(default=10, verbose_name="筛选产品数量")
 
 
 class ECGood(BaseModel):
     part = models.CharField(
         max_length=255, blank=True, default="", verbose_name="零件号"
-    )  # excel的唯一值
-    manufacturer = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="制造商"
-    )  # excel的
+    )  # 唯一值，与GSA的mfr_part_no_gsa对应
     mfr_part_no = models.CharField(
         max_length=255, blank=True, default="", verbose_name="制造商零件号"
     )  # ec网页上的
@@ -103,12 +54,8 @@ class ECGood(BaseModel):
 
 
 class GSAGood(BaseModel):
-    part = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="零件号"
-    )  # excel的唯一值
-    manufacturer_name = models.CharField(
-        max_length=255, blank=True, default="", verbose_name="制造商名称"
-    )  # gsa网页上的
+    brand_name = models.CharField(max_length=255, verbose_name="品牌名称")
+    sin = models.CharField(max_length=255, default="", verbose_name="sin")
     product_name = models.CharField(
         max_length=255, blank=True, default="", verbose_name="产品名称"
     )  # gsa网页上的
